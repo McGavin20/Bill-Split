6e46eb33815cb5d1edead83b23f1258682b78816
Sharma Muridzo <smuridzo20@gmail.com>
2023-03-27 17:00:06 +0200

    App

---
+++ BillSplit/BillSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillSplitApp.swift
+//  BillSplit
+//
+//  Created by Sharma McGavin on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BillSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
